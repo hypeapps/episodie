310e501aa18423289970f1f04be90fc01bc2f358
Przemek <pszem.szym@gmail.com>
2016-08-27 18:31:27 +0200

    Initial commit

---
+++ app/src/main/java/pl/hypeapp/episoder/ui/activity/MainWallActivity.java
@@ -0,0 +1,25 @@
+package pl.hypeapp.episoder.ui.activity;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import pl.hypeapp.episoder.R;
+
+
+public class MainWallActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_wall);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Main Wall");
+        setSupportActionBar(toolbar);
+    }
+}
