6f7ac8490f69f1627d5b7966191ee84df76fce00
Przemek <pszem.szym@gmail.com>
2016-08-28 22:33:14 +0200

    Created FontManager for loding fonts from assets.

---
+++ app/src/main/java/pl/hypeapp/episoder/util/FontManager.java
@@ -0,0 +1,47 @@
+package pl.hypeapp.episoder.util;
+
+
+import android.content.res.AssetManager;
+import android.graphics.Typeface;
+import android.util.Log;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FontManager {
+    private static final String TAG = FontManager.class.getName();
+
+    private static FontManager instance;
+
+    private AssetManager assetManager;
+
+    private Map<String, Typeface> fonts;
+
+    private FontManager(AssetManager assetManager) {
+        this.assetManager = assetManager;
+        this.fonts = new HashMap<>();
+    }
+
+    public static FontManager getInstance(AssetManager assetManager) {
+        if (instance == null) {
+            instance = new FontManager(assetManager);
+        }
+        return instance;
+    }
+
+    public Typeface getFont(String asset) {
+        if (fonts.containsKey(asset))
+            return fonts.get(asset);
+
+        Typeface font = null;
+
+        try {
+            font = Typeface.createFromAsset(assetManager, asset);
+            fonts.put(asset, font);
+        } catch (RuntimeException e) {
+            Log.e(TAG, "getFont: Can't create font from asset.", e);
+        }
+
+        return font;
+    }
+}
