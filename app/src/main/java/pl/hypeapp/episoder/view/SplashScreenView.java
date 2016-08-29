dc735d1d11faa5825c1770f27462f8a78b736107
Przemek <pszem.szym@gmail.com>
2016-08-29 23:41:40 +0200

    Created interface SplashScreenView

---
+++ app/src/main/java/pl/hypeapp/episoder/view/SplashScreenView.java
@@ -0,0 +1,8 @@
+package pl.hypeapp.episoder.view;
+
+import com.hannesdorfmann.mosby.mvp.MvpView;
+
+public interface SplashScreenView extends MvpView{
+
+    void runActivity(Class startActivityClass);
+}
