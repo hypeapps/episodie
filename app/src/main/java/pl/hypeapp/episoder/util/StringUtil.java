fd7e3f1ca14d57dc2f338cae15fa8c18e3cd0654
Przemek <pszem.szym@gmail.com>
2016-08-29 03:15:41 +0200

    Created StringUtil

---
+++ app/src/main/java/pl/hypeapp/episoder/util/StringUtil.java
@@ -0,0 +1,15 @@
+package pl.hypeapp.episoder.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class StringUtil {
+
+    public static String[] upperCaseArray(String[] textArray){
+        List<String> list = new ArrayList();
+        for (String text : textArray) {
+            list.add(text.toUpperCase());
+        }
+        return list.toArray(new String[list.size()]);
+    }
+}
