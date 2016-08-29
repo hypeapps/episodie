6d8ed9dca0062badd00697a0fa51121aaa654a85
Przemek <pszem.szym@gmail.com>
2016-08-29 23:43:02 +0200

    Added new font LeagueGothic-Regular.

---
+++ app/src/main/java/pl/hypeapp/episoder/util/StartActivityUtil.java
@@ -0,0 +1,31 @@
+package pl.hypeapp.episoder.util;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v4.app.ActivityOptionsCompat;
+import android.support.v4.util.Pair;
+import android.view.View;
+
+public class StartActivityUtil {
+    private static StartActivityUtil ourInstance = new StartActivityUtil();
+
+    private static Activity context;
+
+    public static StartActivityUtil getInstance(Activity activity) {
+        context = activity;
+        return ourInstance;
+    }
+
+    private StartActivityUtil() {
+    }
+
+    public void runActivityWithTransition(Class startActivityClass, Pair<View, String>... sharedElements){
+        ActivityOptionsCompat transitionActivityOptions = null;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+            transitionActivityOptions = ActivityOptionsCompat.
+                    makeSceneTransitionAnimation(context, sharedElements);
+        }
+        context.startActivity(new Intent(context, startActivityClass),
+                transitionActivityOptions.toBundle());
+    }
+}
