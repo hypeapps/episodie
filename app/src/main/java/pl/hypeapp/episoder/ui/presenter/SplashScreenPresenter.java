784bf92c1e37f96421e68c19d6d64388faa200c5
Przemek <pszem.szym@gmail.com>
2016-08-30 15:28:14 +0200

    Now the presenter controls when load image + now activity has two methods from interface which control load image from url and then placing image into view.

--- app/src/main/java/pl/hypeapp/episoder/ui/presenter/SplashScreenPresenter.java
+++ app/src/main/java/pl/hypeapp/episoder/ui/presenter/SplashScreenPresenter.java
@@ -1,13 +1,22 @@
 package pl.hypeapp.episoder.ui.presenter;
 
+import android.net.Uri;
 import android.os.Handler;
+import android.widget.ImageView;
 
+import com.bumptech.glide.DrawableTypeRequest;
+import com.bumptech.glide.Glide;
 import com.hannesdorfmann.mosby.mvp.MvpBasePresenter;
 
 import pl.hypeapp.episoder.ui.activity.LoginActivity;
 import pl.hypeapp.episoder.view.SplashScreenView;
 
 public class SplashScreenPresenter extends MvpBasePresenter<SplashScreenView> {
+
+    public void loadBackgroundImage(ImageView backgroundImageView){
+        String url = "http://www.goliath.com/wp-content/uploads/2015/12/1035x776-breakingbad-1800-1404309469.jpg";
+        getView().loadImageIntoView(backgroundImageView, getView().loadImageFromUrl(url));
+    }
 
     public void runActivityWithDelay(long delayInMilis){
         Handler handler = new Handler();
@@ -19,6 +28,5 @@ public class SplashScreenPresenter extends MvpBasePresenter<SplashScreenView> {
                 }
             }
         }, delayInMilis);
-
     }
 }
