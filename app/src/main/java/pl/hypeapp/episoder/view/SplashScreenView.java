55b88c0050748d03ec039cf04b78bca199a93f86
Przemek <pszem.szym@gmail.com>
2016-08-30 15:26:11 +0200

    Created new interfaces for loading image with Glide

--- app/src/main/java/pl/hypeapp/episoder/view/SplashScreenView.java
+++ app/src/main/java/pl/hypeapp/episoder/view/SplashScreenView.java
@@ -1,8 +1,17 @@
 package pl.hypeapp.episoder.view;
 
+import android.net.Uri;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.bumptech.glide.DrawableTypeRequest;
 import com.hannesdorfmann.mosby.mvp.MvpView;
 
 public interface SplashScreenView extends MvpView{
 
     void runActivity(Class startActivityClass);
+
+    DrawableTypeRequest loadImageFromUrl(String url);
+
+    void loadImageIntoView(ImageView view, DrawableTypeRequest drawableTypeRequest);
 }
