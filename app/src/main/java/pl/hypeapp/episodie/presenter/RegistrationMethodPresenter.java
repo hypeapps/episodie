559430f0b9086179e0c6b3e81c9c757034f2ca25
PrzemekEnterprise <pszem.szym@gmail.com>
2017-05-20 14:18:39 +0200

    Deleted whole package app.

--- app/src/main/java/pl/hypeapp/episodie/presenter/RegistrationMethodPresenter.java
+++
@@ -1,8 +0,0 @@
-package pl.hypeapp.episodie.presenter;
-
-import net.grandcentrix.thirtyinch.TiPresenter;
-
-import pl.hypeapp.episodie.view.RegistrationMethodView;
-
-public class RegistrationMethodPresenter extends TiPresenter<RegistrationMethodView>{
-}
