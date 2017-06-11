1df38c875a68e6869cf707ed415e23832d8ba5eb
PrzemekEnterprise <pszem.szym@gmail.com>
2017-06-11 17:52:40 +0200

    Refactor: added whitespaces.

--- application/src/main/kotlin/pl/hypeapp/episodie/di/components/AppComponent.kt
+++ application/src/main/kotlin/pl/hypeapp/episodie/di/components/AppComponent.kt
@@ -6,13 +6,13 @@ import pl.hypeapp.domain.executor.PostExecutionThread
 import pl.hypeapp.domain.executor.ThreadExecutor
 import pl.hypeapp.domain.repository.Repository
 import pl.hypeapp.episodie.App
+import pl.hypeapp.episodie.di.module.ApiModule
 import pl.hypeapp.episodie.di.module.AppModule
 import pl.hypeapp.episodie.di.module.CacheModule
-import pl.hypeapp.episodie.di.module.NetworkModule
 import javax.inject.Singleton
 
 @Singleton
-@Component(modules = arrayOf(AppModule::class, NetworkModule::class, CacheModule::class))
+@Component(modules = arrayOf(AppModule::class, ApiModule::class, CacheModule::class))
 interface AppComponent {
 
     fun inject(app: App)
