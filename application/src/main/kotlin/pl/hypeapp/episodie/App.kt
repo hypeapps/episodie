655a0968a2438826128046fc43e130e6beb08ffd
PrzemekEnterprise <pszem.szym@gmail.com>
2017-06-11 17:53:53 +0200

    Add: implemented companion object with static credentials for dagger api module.

--- application/src/main/kotlin/pl/hypeapp/episodie/App.kt
+++ application/src/main/kotlin/pl/hypeapp/episodie/App.kt
@@ -4,9 +4,9 @@ import android.app.Application
 import com.facebook.stetho.Stetho
 import pl.hypeapp.episodie.di.components.AppComponent
 import pl.hypeapp.episodie.di.components.DaggerAppComponent
+import pl.hypeapp.episodie.di.module.ApiModule
 import pl.hypeapp.episodie.di.module.AppModule
 import pl.hypeapp.episodie.di.module.CacheModule
-import pl.hypeapp.episodie.di.module.NetworkModule
 
 class App : Application() {
 
@@ -14,7 +14,7 @@ class App : Application() {
         get() = DaggerAppComponent.builder()
                 .appModule(AppModule(this))
                 .cacheModule(CacheModule(this))
-                .networkModule(NetworkModule("http://api.tvmaze.com/"))
+                .apiModule(ApiModule(URL_API_ENDPOINT, USERNAME, PASSWORD))
                 .build()
 
     override fun onCreate() {
@@ -26,4 +26,10 @@ class App : Application() {
         Stetho.initializeWithDefaults(this)
     }
 
+    companion object {
+        val URL_API_ENDPOINT = "https://episodie.ct8.pl/api/"
+        // YOU NEED TO SPECIFY OWN USERNAME AND PASSWORD IN gradle.properties
+        val USERNAME = BuildConfig.USERNAME_EPISODIE_API
+        val PASSWORD = BuildConfig.PASSWORD_EPISODIE_API
+    }
 }
