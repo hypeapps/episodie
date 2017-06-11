36966b395a91e102eab75cc43ab2284e875adc44
PrzemekEnterprise <pszem.szym@gmail.com>
2017-06-11 16:16:25 +0200

    Deleted: unused block canary context.

--- application/src/main/kotlin/pl/hypeapp/episodie/AppBlockCanaryContext.kt
+++
@@ -1,6 +0,0 @@
-package pl.hypeapp.episodie
-
-import android.content.Context
-import com.exyui.android.debugbottle.ui.BlockCanaryContext
-
-class AppBlockCanaryContext(context: Context) : BlockCanaryContext(context)
