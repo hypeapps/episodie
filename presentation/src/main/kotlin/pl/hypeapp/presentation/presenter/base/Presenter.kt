945e6b414f7d5fb1927e8ea6545ba8247e35cc3c
PrzemekEnterprise <pszem.szym@gmail.com>
2017-06-11 15:00:26 +0200

    Refactor: Changed implementation for base presenter and changed name of BaseView class to simply View.

--- presentation/src/main/kotlin/pl/hypeapp/presentation/presenter/base/Presenter.kt
+++ presentation/src/main/kotlin/pl/hypeapp/presentation/presenter/base/Presenter.kt
@@ -1,18 +1,16 @@
 package pl.hypeapp.presentation.presenter.base
 
-import pl.hypeapp.presentation.view.base.BaseView
+import pl.hypeapp.presentation.view.base.View
 
-abstract class Presenter<V : BaseView> {
+open class Presenter<V : View> {
 
     var view: V? = null
 
-    open fun onCreate(view: V) {
+    open fun onAttachView(view: V) {
         this.view = view
     }
 
-    abstract fun onResume()
-
-    abstract fun onPause()
-
-    abstract fun onDestroy()
+    open fun onDetachView() {
+        this.view = null
+    }
 }
