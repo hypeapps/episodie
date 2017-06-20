b998819566c59f770650d5e3206ffef085da935d
PrzemekEnterprise <pszem.szym@gmail.com>
2017-06-20 21:59:27 +0200

    Add: abstraction for delegate adapters.

---
+++ application/src/main/kotlin/pl/hypeapp/episodie/adapter/AdapterConstants.kt
@@ -0,0 +1,9 @@
+package pl.hypeapp.episodie.adapter
+
+object AdapterConstants {
+
+    val ITEM = 1
+
+    val LOADING = 2
+
+}
---
+++ application/src/main/kotlin/pl/hypeapp/episodie/adapter/ViewType.kt
@@ -0,0 +1,7 @@
+package pl.hypeapp.episodie.adapter
+
+interface ViewType {
+
+    fun getViewType(): Int
+
+}
