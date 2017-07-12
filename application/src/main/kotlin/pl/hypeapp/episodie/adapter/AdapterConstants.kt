32ce83e2e04a4c280836d7513c1da43631599632
PrzemekEnterprise <pszem.szym@gmail.com>
2017-07-13 00:03:48 +0200

    Refactor: adapter constants are now in ViewType.

--- application/src/main/kotlin/pl/hypeapp/episodie/adapter/AdapterConstants.kt
+++
@@ -1,9 +0,0 @@
-package pl.hypeapp.episodie.adapter
-
-object AdapterConstants {
-
-    val ITEM = 1
-
-    val LOADING = 2
-
-}
--- application/src/main/kotlin/pl/hypeapp/episodie/adapter/ViewType.kt
+++ application/src/main/kotlin/pl/hypeapp/episodie/adapter/ViewType.kt
@@ -4,4 +4,14 @@ interface ViewType {
 
     fun getViewType(): Int
 
+    companion object {
+
+        val ITEM = 1
+
+        val LOADING = 2
+
+        val ERROR = 3
+
+    }
+
 }
