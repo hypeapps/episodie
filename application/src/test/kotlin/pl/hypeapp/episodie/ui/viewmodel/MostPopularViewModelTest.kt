0b1e20f88a0cb28afc77bd5339f4bae3c5e4824e
PrzemekEnterprise <pszem.szym@gmail.com>
2017-07-12 23:53:59 +0200

    Add: most popular view model and test.

---
+++ application/src/test/kotlin/pl/hypeapp/episodie/ui/viewmodel/MostPopularViewModelTest.kt
@@ -0,0 +1,85 @@
+package pl.hypeapp.episodie.ui.viewmodel
+
+import com.nhaarman.mockito_kotlin.given
+import com.nhaarman.mockito_kotlin.verify
+import com.nhaarman.mockito_kotlin.verifyZeroInteractions
+import org.amshove.kluent.`should equal to`
+import org.amshove.kluent.`should equal`
+import org.amshove.kluent.mock
+import org.junit.Before
+import org.junit.Test
+import pl.hypeapp.domain.model.MostPopularModel
+import pl.hypeapp.domain.model.PageableRequest
+import pl.hypeapp.domain.model.TvShowModel
+
+class MostPopularViewModelTest {
+
+    lateinit var mostPopularViewModel: MostPopularViewModel
+
+    private val mostPopularModel: MostPopularModel = mock()
+
+    private val pageableRequest: PageableRequest = mock()
+
+    @Before
+    fun setUp() {
+        mostPopularViewModel = MostPopularViewModel()
+        given(mostPopularModel.pageableRequest).willReturn(pageableRequest)
+    }
+
+    @Test
+    fun `should clear and retain model`() {
+        mostPopularViewModel.clearAndRetainModel(mostPopularModel)
+
+        mostPopularViewModel.retainedModel `should equal` mostPopularModel
+        mostPopularViewModel.page `should equal` 0
+        mostPopularViewModel.tvShowList `should equal` mostPopularModel.tvShows
+    }
+
+    @Test
+    fun `should retain model`() {
+        val model = createFakeModel()
+        mostPopularViewModel.retainModel(model)
+
+        mostPopularViewModel.retainedModel `should equal` model
+        mostPopularViewModel.page `should equal to` 2
+    }
+
+
+    @Test
+    fun `should populate recycler while model is not null`() {
+        val model = createFakeModel()
+        mostPopularViewModel.retainedModel = model
+        val populateRecycler: () -> Unit = mock()
+        val requestMostPopular: () -> Unit = mock()
+
+        mostPopularViewModel.loadModel(requestMostPopular, populateRecycler)
+        verify(populateRecycler).invoke()
+        verifyZeroInteractions(requestMostPopular)
+    }
+
+    @Test
+    fun `should request most popular while model is null`() {
+        mostPopularViewModel.retainedModel = null
+        val populateRecycler: () -> Unit = mock()
+        val requestMostPopular: () -> Unit = mock()
+
+        mostPopularViewModel.loadModel(requestMostPopular, populateRecycler)
+        verify(requestMostPopular).invoke()
+        verifyZeroInteractions(populateRecycler)
+    }
+
+    private fun createFakeModel(): MostPopularModel {
+        val tvShowModel = MostPopularModel(arrayListOf(TvShowModel(
+                id = "2",
+                episodeRuntime = 200L,
+                fullRuntime = 200L,
+                imageMedium = "http://fake.jpg",
+                imageOriginal = "http://fakeOriginal.jpg",
+                imdbId = "tt2222",
+                name = "example show",
+                premiered = "2017-11-11"
+        )), PageableRequest(page = 2, size = 15))
+        return tvShowModel
+    }
+
+}
