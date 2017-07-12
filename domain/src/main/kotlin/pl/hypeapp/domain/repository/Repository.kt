ad3279c62e8e72ba3cc5068a76e3795c4a89f6fd
PrzemekEnterprise <pszem.szym@gmail.com>
2017-07-12 23:27:06 +0200

    Add: most popular repository. Deleted: generic repository.

--- domain/src/main/kotlin/pl/hypeapp/domain/repository/Repository.kt
+++
@@ -1,8 +0,0 @@
-package pl.hypeapp.domain.repository
-
-import io.reactivex.Single
-import pl.hypeapp.domain.model.TvShowModel
-
-interface Repository {
-    fun getTvShow(tvShowId: String): Single<TvShowModel>
-}
