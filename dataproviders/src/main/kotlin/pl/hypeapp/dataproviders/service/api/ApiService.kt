425aad72d20af88d90e2011761d08ce25a1a5ed9
PrzemekEnterprise <pszem.szym@gmail.com>
2017-07-12 23:44:44 +0200

    Add: missing api service class

---
+++ dataproviders/src/main/kotlin/pl/hypeapp/dataproviders/service/api/ApiService.kt
@@ -0,0 +1,29 @@
+package pl.hypeapp.dataproviders.service.api
+
+import io.reactivex.Single
+import pl.hypeapp.dataproviders.entity.MostPopularEntity
+import pl.hypeapp.dataproviders.entity.TopListEntity
+import pl.hypeapp.dataproviders.entity.TvShowEntity
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class ApiService @Inject constructor(private val episodieApi: EpisodieApi) : EpisodieApi {
+
+    override fun getMostPopular(page: Int, size: Int): Single<MostPopularEntity> {
+        return episodieApi.getMostPopular(page, size)
+    }
+
+    override fun getTopList(page: Int, size: Int): Single<TopListEntity> {
+        return episodieApi.getTopList(page, size)
+    }
+
+    override fun search(query: String): Single<List<TvShowEntity>> {
+        return episodieApi.search(query)
+    }
+
+    override fun getTvShow(tvShowId: String): Single<TvShowEntity> {
+        return episodieApi.getTvShow(tvShowId)
+    }
+
+}
