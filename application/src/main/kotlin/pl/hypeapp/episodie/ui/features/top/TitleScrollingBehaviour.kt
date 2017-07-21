a89ed1c2b239af951f493f28892e587f2c48ce05
PrzemekEnterprise <pszem.szym@gmail.com>
2017-07-21 18:52:42 +0200

    Add: title scrolling behaviour.

---
+++ application/src/main/kotlin/pl/hypeapp/episodie/ui/features/top/TitleScrollingBehaviour.kt
@@ -0,0 +1,27 @@
+package pl.hypeapp.episodie.ui.features.top
+
+import android.content.Context
+import android.support.design.widget.AppBarLayout
+import android.support.design.widget.CoordinatorLayout
+import android.util.AttributeSet
+import android.view.View
+import android.widget.TextView
+import pl.hypeapp.episodie.R
+
+class TitleScrollingBehaviour(context: Context, attrs: AttributeSet) : AppBarLayout.ScrollingViewBehavior(context, attrs) {
+
+    override fun layoutDependsOn(parent: CoordinatorLayout?, child: View?, dependency: View?): Boolean {
+        super.layoutDependsOn(parent, child, dependency)
+        return dependency is AppBarLayout
+    }
+
+    override fun onDependentViewChanged(parent: CoordinatorLayout?, child: View?, dependency: View?): Boolean {
+        val scrollPercentage = (dependency?.y!!.toFloat() / dependency.height) * -1
+        with(dependency.findViewById<TextView>(R.id.appBartext)) {
+            scaleY = 1f - scrollPercentage / 2.5f
+            scaleX = 1f - scrollPercentage / 2.5f
+        }
+        return super.onDependentViewChanged(parent, child, dependency)
+    }
+
+}
