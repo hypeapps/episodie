79110a75517f64ea56c29c3b12593334e86f70d7
PrzemekEnterprise <pszem.szym@gmail.com>
2017-07-21 18:32:17 +0200

    Add: get top list. Add: test for get top list. Add: get top list cache provider.

--- dataproviders/src/main/kotlin/pl/hypeapp/dataproviders/datasource/DataSource.kt
+++ dataproviders/src/main/kotlin/pl/hypeapp/dataproviders/datasource/DataSource.kt
@@ -8,6 +8,7 @@ import io.rx_cache2.EvictDynamicKeyGroup
 import pl.hypeapp.dataproviders.cache.CacheProviders
 import pl.hypeapp.dataproviders.cache.EvictCache
 import pl.hypeapp.dataproviders.entity.MostPopularEntity
+import pl.hypeapp.dataproviders.entity.TopListEntity
 import pl.hypeapp.dataproviders.entity.TvShowEntity
 import pl.hypeapp.dataproviders.service.api.ApiService
 import pl.hypeapp.domain.model.PageableRequest
@@ -23,11 +24,22 @@ class DataSource @Inject constructor(private val apiService: ApiService,
             cacheProviders.getTvShow(apiService.getTvShow(tvShowId), DynamicKey(tvShowId), EvictDynamicKey(update))
 
     override fun getMostPopular(pageableRequest: PageableRequest, update: Boolean): Single<MostPopularEntity> {
-        if (update) evictCache.evictAllMatchingDynamicKey(DynamicKey("MP"))
-        return cacheProviders
-                .getMostPopular(apiService.getMostPopular(pageableRequest.page,
-                        pageableRequest.size), DynamicKeyGroup("MP", pageableRequest.page),
+        if (update) evictCache.evictAllMatchingDynamicKey(DynamicKey(MOST_POPULAR_KEY))
+        return cacheProviders.getMostPopular(apiService.getMostPopular(pageableRequest.page,
+                pageableRequest.size), DynamicKeyGroup(MOST_POPULAR_KEY, pageableRequest.page),
                         EvictDynamicKeyGroup(update))
+    }
+
+    override fun getTopList(pageableRequest: PageableRequest, update: Boolean): Single<TopListEntity> {
+        if (update) evictCache.evictAllMatchingDynamicKey(DynamicKey(TOP_LIST_KEY))
+        return cacheProviders.getTopList(apiService.getTopList(pageableRequest.page,
+                pageableRequest.size), DynamicKeyGroup(TOP_LIST_KEY, pageableRequest.page),
+                EvictDynamicKeyGroup(update))
+    }
+
+    private companion object {
+        val MOST_POPULAR_KEY = "MP"
+        val TOP_LIST_KEY = "TL"
     }
 
 }
