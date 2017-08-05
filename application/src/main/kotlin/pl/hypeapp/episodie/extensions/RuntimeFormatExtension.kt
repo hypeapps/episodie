1b8520fa2ff161bd4028e5f4c9eddc61b718d7ab
PrzemekEnterprise <pszem.szym@gmaill.com>
2017-08-05 16:56:48 +0200

    Refactor: minor

--- application/src/main/kotlin/pl/hypeapp/episodie/extensions/RuntimeFormatExtension.kt
+++ application/src/main/kotlin/pl/hypeapp/episodie/extensions/RuntimeFormatExtension.kt
@@ -23,20 +23,17 @@ fun TextView.setFullRuntime(runtime: Long?) {
         val days: Long = MINUTES.toDays(it)
         val hours = MINUTES.toHours((it - DAYS.toMinutes(days)))
         val minutes = MINUTES.toMinutes(it - HOURS.toMinutes(hours) - DAYS.toMinutes(days))
-        var minutesFormatted: String = minutes.toString()
-        if (minutes < 10) {
-            minutesFormatted = "0$minutes"
-        }
+        val minutesFormatted: String = if (minutes < 10) minutes.toString() else "0$minutes"
         if (isUpToOneHour(it)) {
             this.text = String.format(resources.getString(R.string.time_unit_format_minutes), it)
         } else if (isAtLeastOneDay(it)) {
-            if (isMinutesOnlyZeros(minutesFormatted)) {
+            if (isMinutesZerosOnly(minutesFormatted)) {
                 this.text = String.format(resources.getString(R.string.time_unit_format_days_hours), days, hours)
             } else {
                 this.text = String.format(resources.getString(R.string.time_unit_format_full), days, hours, minutesFormatted)
             }
         } else {
-            if (isMinutesOnlyZeros(minutesFormatted)) {
+            if (isMinutesZerosOnly(minutesFormatted)) {
                 this.text = String.format(resources.getString(R.string.time_unit_format_hours), hours)
             } else {
                 this.text = String.format(resources.getString(R.string.time_unit_format_hours_minutes), hours, minutesFormatted)
@@ -51,4 +48,4 @@ private fun isLessThanOneDay(it: Long) = it <= MINUTES.convert(24 * 60, MINUTES)
 
 private fun isUpToOneHour(runtime: Long) = runtime <= MINUTES.convert(60, MINUTES)
 
-private fun isMinutesOnlyZeros(minutesFormatted: String) = minutesFormatted == "00"
+private fun isMinutesZerosOnly(minutesFormatted: String) = minutesFormatted == "00"
