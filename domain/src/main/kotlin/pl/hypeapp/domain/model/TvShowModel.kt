9de2fc5b3cbf84439e135c39eae705714c062103
Przemek <pszem.szym@gmail.com>
2017-08-17 20:02:08 +0200

    Deleted: moved class. Add: navigator. Add: domain models. Add: missing tv show info fragment.

--- domain/src/main/kotlin/pl/hypeapp/domain/model/TvShowModel.kt
+++ domain/src/main/kotlin/pl/hypeapp/domain/model/TvShowModel.kt
@@ -1,11 +1,17 @@
 package pl.hypeapp.domain.model
 
-data class TvShowModel(
-        val id: String?,
-        val imdbId: String?,
-        val name: String?,
-        val episodeRuntime: Long?,
-        val fullRuntime: Long?,
-        val premiered: String?,
-        val imageMedium: String?,
-        val imageOriginal: String?)
+import java.io.Serializable
+
+data class TvShowModel(val id: String?,
+                       val imdbId: String?,
+                       val name: String?,
+                       val network: String?,
+                       val status: String?,
+                       val summary: String?,
+                       val episodeRuntime: Long?,
+                       val genre: String?,
+                       val officialSite: String?,
+                       val fullRuntime: Long?,
+                       val premiered: String?,
+                       val imageMedium: String?,
+                       val imageOriginal: String?) : Serializable
