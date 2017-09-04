322ea28343738a083185cf4a4d6ebadc67f8600c
Przemek <pszem.szym@gmail.com>
2017-09-04 21:15:54 +0200

    Add: add to watched logic to top list.

--- application/src/main/kotlin/pl/hypeapp/episodie/ui/features/top/adapter/TopListDelegateAdapter.kt
+++ application/src/main/kotlin/pl/hypeapp/episodie/ui/features/top/adapter/TopListDelegateAdapter.kt
@@ -3,17 +3,16 @@ package pl.hypeapp.episodie.ui.features.top.adapter
 import android.support.v4.content.ContextCompat
 import android.support.v7.widget.RecyclerView
 import android.view.ViewGroup
+import android.widget.ImageView
 import kotlinx.android.synthetic.main.item_top_list.view.*
+import pl.hypeapp.domain.model.TvShowModel
 import pl.hypeapp.episodie.R
-import pl.hypeapp.episodie.extensions.inflate
-import pl.hypeapp.episodie.extensions.setRuntime
-import pl.hypeapp.episodie.extensions.setZeroPrefixUnderTen
-import pl.hypeapp.episodie.glide.GlideApp
+import pl.hypeapp.episodie.extensions.*
 import pl.hypeapp.episodie.ui.base.adapter.ViewType
 import pl.hypeapp.episodie.ui.base.adapter.ViewTypeDelegateAdapter
 import pl.hypeapp.episodie.ui.viewmodel.TvShowViewModel
 
-class TopListDelegateAdapter(val onViewSelectedLister: TopListOnViewSelectedListener) : ViewTypeDelegateAdapter {
+class TopListDelegateAdapter(val onViewSelectedListener: TopListOnViewSelectedListener) : ViewTypeDelegateAdapter {
 
     override fun onCreateViewHolder(parent: ViewGroup): RecyclerView.ViewHolder = TopListViewHolder(parent)
 
@@ -32,20 +31,24 @@ class TopListDelegateAdapter(val onViewSelectedLister: TopListOnViewSelectedList
             }
             text_view_top_list_position.setZeroPrefixUnderTen(adapterPosition + 1)
             item.tvShow?.let {
-                GlideApp.with(itemView)
-                        .load(it.imageMedium)
-                        .into(image_view_item_top_list_cover)
+                image_view_item_top_list_cover.loadImage(it.imageMedium)
                 text_view_item_top_list_title.text = it.name
                 text_view_item_top_list_runtime.setRuntime(it.fullRuntime)
-                ripple_view_item_top_list.setOnRippleCompleteListener {
-                    onViewSelectedLister.onItemSelected(
-                            item.tvShow,
-                            image_view_item_top_list_cover,
-                            text_view_item_top_list_title,
-                            text_view_item_top_list_runtime)
+                ripple_view_item_top_list.setOnRippleCompleteListener { onViewSelected(item.tvShow) }
+                image_view_item_top_list_diamond.setOnClickListener { view ->
+                    onViewSelectedListener.onChangeWatchState(it, view as ImageView)
                 }
-                image_view_item_top_list_diamond.setOnClickListener { onViewSelectedLister.onAddToWatched(item.tvShow.id!!) }
+                image_view_item_top_list_diamond.manageWatchStateIcon(it.watchState)
             }
         }
+
+        private fun onViewSelected(tvShowModel: TvShowModel) = with(itemView) {
+            onViewSelectedListener.onItemSelected(
+                    tvShowModel,
+                    image_view_item_top_list_cover,
+                    text_view_item_top_list_title,
+                    text_view_item_top_list_runtime)
+        }
+
     }
 }
--- application/src/main/kotlin/pl/hypeapp/episodie/ui/features/top/adapter/TopListOnViewSelectedListener.kt
+++ application/src/main/kotlin/pl/hypeapp/episodie/ui/features/top/adapter/TopListOnViewSelectedListener.kt
@@ -1,9 +1,11 @@
 package pl.hypeapp.episodie.ui.features.top.adapter
 
+import android.widget.ImageView
+import pl.hypeapp.domain.model.TvShowModel
 import pl.hypeapp.episodie.ui.base.adapter.ViewTypeDelegateAdapter
 
 interface TopListOnViewSelectedListener : ViewTypeDelegateAdapter.OnViewSelectedListener {
 
-    fun onAddToWatched(tvShowId: String)
+    fun onChangeWatchState(tvShow: TvShowModel, diamondIcon: ImageView)
 
 }
