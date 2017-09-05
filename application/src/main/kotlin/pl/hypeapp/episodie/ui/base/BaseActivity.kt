c286cfcd4d346f6631fd8043127f32e23bc46d84
Przemek <pszem.szym@gmail.com>
2017-09-05 16:39:48 +0200

    Add: small bang animator to base activity and fragment.

--- application/src/main/kotlin/pl/hypeapp/episodie/ui/base/BaseActivity.kt
+++ application/src/main/kotlin/pl/hypeapp/episodie/ui/base/BaseActivity.kt
@@ -1,12 +1,11 @@
 package pl.hypeapp.episodie.ui.base
 
-import android.arch.lifecycle.LifecycleRegistry
-import android.arch.lifecycle.LifecycleRegistryOwner
 import android.content.Context
 import android.os.Bundle
 import android.support.annotation.LayoutRes
 import android.support.v7.app.AppCompatActivity
 import butterknife.ButterKnife
+import pl.hypeapp.episodie.ui.animation.SmallBangAnimator
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper
 
 @Suppress("LeakingThis")
@@ -19,10 +18,17 @@ abstract class BaseActivity : AppCompatActivity(), LifecycleRegistryOwner {
 
     override fun getLifecycle(): LifecycleRegistry = lifecycleRegistry
 
+    protected lateinit var smallBangAnimator: SmallBangAnimator
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(getLayoutRes())
         ButterKnife.bind(this)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        smallBangAnimator = SmallBangAnimator(this)
     }
 
     override fun attachBaseContext(newBase: Context?) {
