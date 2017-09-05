5bb0df04c23697a32b2378d6c4116efc6bdd6680
PrzemekEnterprise <pszem.szym@gmaill.com>
2017-09-05 16:23:50 +0200

    Add: watch state logic for episodes and seasons.

--- application/src/main/kotlin/pl/hypeapp/episodie/ui/features/seasons/adapter/SeasonItemHolder.kt
+++ application/src/main/kotlin/pl/hypeapp/episodie/ui/features/seasons/adapter/SeasonItemHolder.kt
@@ -1,5 +1,6 @@
 package pl.hypeapp.episodie.ui.features.seasons.adapter
 
+import android.widget.ImageView
 import com.xwray.groupie.ExpandableGroup
 import com.xwray.groupie.ExpandableItem
 import com.xwray.groupie.Item
@@ -8,10 +9,13 @@ import kotlinx.android.synthetic.main.item_season.view.*
 import pl.hypeapp.domain.model.SeasonModel
 import pl.hypeapp.episodie.R
 import pl.hypeapp.episodie.extensions.loadImage
+import pl.hypeapp.episodie.extensions.manageWatchStateIcon
 import pl.hypeapp.episodie.extensions.setFullRuntime
 import pl.hypeapp.episodie.extensions.setSPrefix
 
-class SeasonItemHolder constructor(val seasonsModel: SeasonModel, val onToggleGroupListener: OnToggleGroupListener)
+class SeasonItemHolder constructor(private val seasonsModel: SeasonModel,
+                                   private val onToggleGroupListener: OnToggleGroupListener,
+                                   private val onChangeWatchStateListener: OnChangeWatchStateListener)
     : Item<ViewHolder>(), ExpandableItem {
 
     private lateinit var expandableGroup: ExpandableGroup
@@ -22,7 +26,11 @@ class SeasonItemHolder constructor(val seasonsModel: SeasonModel, val onToggleGr
         viewHolder.itemView.ripple_view_item_season.setOnClickListener {
             onToggleGroupListener.onToggleExpandableGroup(expandableGroup)
         }
+        viewHolder.itemView.image_view_item_season_add_to_watched.setOnClickListener { view ->
+            onChangeWatchStateListener.onChangeSeasonWatchState(seasonsModel, view as ImageView)
+        }
         bindData(viewHolder)
+        viewHolder.setIsRecyclable(false)
     }
 
     override fun setExpandableGroup(onToggleListener: ExpandableGroup) {
@@ -40,6 +48,7 @@ class SeasonItemHolder constructor(val seasonsModel: SeasonModel, val onToggleGr
         text_view_item_season_runtime.setFullRuntime(seasonsModel.runtime)
         text_view_item_season_episode_order.text = String.format(resources.getString(R.string.item_season_order_format),
                 seasonsModel.episodeOrder)
+        image_view_item_season_add_to_watched.manageWatchStateIcon(seasonsModel.watchState)
     }
 
 }
