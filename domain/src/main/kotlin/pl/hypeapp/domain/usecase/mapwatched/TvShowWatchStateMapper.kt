97725be60e30d5ffe7a177c2bcd9afc574574ec7
Przemek <pszem.szym@gmail.com>
2017-09-17 15:53:10 +0200

    Refactor: deleted logs and added some comments.

--- domain/src/main/kotlin/pl/hypeapp/domain/usecase/mapwatched/TvShowWatchStateMapper.kt
+++ domain/src/main/kotlin/pl/hypeapp/domain/usecase/mapwatched/TvShowWatchStateMapper.kt
@@ -2,24 +2,20 @@ package pl.hypeapp.domain.usecase.mapwatched
 
 import pl.hypeapp.domain.model.*
 import pl.hypeapp.domain.repository.WatchedRepository
-import java.util.logging.Logger
 import javax.inject.Inject
 
 class TvShowWatchStateMapper @Inject constructor(private val repository: WatchedRepository) {
 
-    val log: Logger = Logger.getAnonymousLogger()
-
-    fun map(tvShowId: String) {
-//        log.info(" COUNT " + repository.getWatchedEpisodesCount(id))
-    }
-
     fun map(seasonsModel: AllSeasonsModel) = seasonsModel.seasons?.let {
+        // get seasons ids for repository call
         val seasonsIds: List<String> = it.map { it.seasonId!! }
+        // map all episodes watch state to not watched
         it.forEach {
             it.watchState = WatchState.NOT_WATCHED
             it.episodes?.map { it.watchState = WatchState.NOT_WATCHED }
         }
         val watchedSeasonsCount: List<WatchedEpisodesCountModel> = repository.getWatchedSeasonsCountByIds(seasonsIds)
+        // apply proper season and episode watch state for all seasons which are present in repository
         watchedSeasonsCount.forEach { (id, count) ->
             it.filter { it.seasonId == id }
                     .map {
@@ -27,26 +23,15 @@ class TvShowWatchStateMapper @Inject constructor(private val repository: Watched
                         applyProperEpisodeWatchState(it)
                     }
         }
-        it.forEach {
-            log.info(" SESAONS INFO " + it.seasonNumber + " watch state " + it.watchState)
-        }
     }
 
     fun map(tvShows: List<TvShowModel>) = tvShows.let {
         val tvShowsIds: List<String> = it.map { it.id!! }
-        log.info(" SIZE " + tvShowsIds.size)
-        log.info(" SIZE REPO " + repository.getWatchedEpisodesCountByTvShowIds(tvShowsIds).size)
         val watchedEpisodesCount: List<WatchedEpisodesCountModel> = repository.getWatchedEpisodesCountByTvShowIds(tvShowsIds)
         tvShows.forEach { it.watchState = WatchState.NOT_WATCHED }
         watchedEpisodesCount.forEach { (tvShowId, count) ->
             it.filter { it.id == tvShowId }
-                    .map {
-                        log.info("tv model " + it.episodeOrder + " count " + count)
-                        applyProperTvShowWatchState(count, it)
-                    }
-        }
-        tvShows.forEach {
-            log.info(" INFO " + it.name + " " + it.watchState)
+                    .map { applyProperTvShowWatchState(count, it) }
         }
     }
 
