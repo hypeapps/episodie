7aead2f0b16ce5265f0d438e1bbce239f7717966
Przemek <pszem.szym@gmail.com>
2017-10-30 19:09:44 +0100

    Add: disposable to base activity.

--- application/src/main/kotlin/pl/hypeapp/episodie/ui/base/BaseActivity.kt
+++ application/src/main/kotlin/pl/hypeapp/episodie/ui/base/BaseActivity.kt
@@ -1,26 +1,24 @@
 package pl.hypeapp.episodie.ui.base
 
-import android.arch.lifecycle.LifecycleRegistry
-import android.arch.lifecycle.LifecycleRegistryOwner
 import android.content.Context
 import android.os.Bundle
 import android.support.annotation.LayoutRes
 import android.support.v7.app.AppCompatActivity
+
 import butterknife.ButterKnife
+import io.reactivex.disposables.Disposable
 import pl.hypeapp.episodie.ui.animation.SmallBangAnimator
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper
 
 @Suppress("LeakingThis")
-abstract class BaseActivity : AppCompatActivity(), LifecycleRegistryOwner {
+abstract class BaseActivity : AppCompatActivity() {
 
     @LayoutRes
     protected abstract fun getLayoutRes(): Int
 
-    private val lifecycleRegistry: LifecycleRegistry = LifecycleRegistry(this)
-
-    override fun getLifecycle(): LifecycleRegistry = lifecycleRegistry
-
     protected lateinit var smallBangAnimator: SmallBangAnimator
+
+    protected var disposable: Disposable? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,5 +34,10 @@ abstract class BaseActivity : AppCompatActivity(), LifecycleRegistryOwner {
     override fun attachBaseContext(newBase: Context?) {
         super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase))
     }
+
+    protected fun disposeSubscription() = disposable?.let {
+        if (!it.isDisposed) it.dispose()
+    }
+
 
 }
