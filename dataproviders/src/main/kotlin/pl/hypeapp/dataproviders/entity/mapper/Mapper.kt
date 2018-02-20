48b13c3f926910c9a09d83ae6ca000c1f38ecebf
Przemek <pszem.szym@gmail.com>
2018-02-20 23:49:50 +0100

    Refactor: rename mapper.

--- dataproviders/src/main/kotlin/pl/hypeapp/dataproviders/entity/mapper/Mapper.kt
+++ dataproviders/src/main/kotlin/pl/hypeapp/dataproviders/entity/mapper/Mapper.kt
@@ -1,12 +1,12 @@
 package pl.hypeapp.dataproviders.entity.mapper
 
-abstract class EntityMapper<Model, Entity> {
+abstract class Mapper<Out, In> {
 
-    abstract fun transform(entity: Entity?): Model?
+    abstract fun transform(item: In?): Out?
 
-    fun transform(collection: Collection<Entity>): List<Model> {
-        val list = ArrayList<Model>()
-        var model: Model?
+    fun transform(collection: Collection<In>): List<Out> {
+        val list = ArrayList<Out>()
+        var model: Out?
         for (entity in collection) {
             model = transform(entity)
             if (model != null) {
