be102b8e30cc9a403159057c7eb6645eb74d2f87
Przemek <pszem.szym@gmail.com>
2018-03-22 20:14:52 +0100

    Add: function for validate context for glide load.

--- application/src/main/kotlin/pl/hypeapp/episodie/glide/MyGlideApp.kt
+++ application/src/main/kotlin/pl/hypeapp/episodie/glide/MyGlideApp.kt
@@ -1,7 +1,22 @@
 package pl.hypeapp.episodie.glide
 
+import android.app.Activity
+import android.content.Context
 import com.bumptech.glide.annotation.GlideModule
 import com.bumptech.glide.module.AppGlideModule
 
+
 @GlideModule
 class MyGlideApp : AppGlideModule()
+
+fun isValidContextForGlide(context: Context?): Boolean {
+    if (context == null) {
+        return false
+    }
+    if (context is Activity) {
+        if (context.isDestroyed || context.isFinishing) {
+            return false
+        }
+    }
+    return true
+}
