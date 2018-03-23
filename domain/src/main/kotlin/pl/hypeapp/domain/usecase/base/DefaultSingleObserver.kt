e8d908aa73c39f017c75d89016fc5cd66bb89df2
Przemek <pszem.szym@gmail.com>
2018-03-23 17:17:30 +0100

    Add: logging in base single observer

--- domain/src/main/kotlin/pl/hypeapp/domain/usecase/base/DefaultSingleObserver.kt
+++ domain/src/main/kotlin/pl/hypeapp/domain/usecase/base/DefaultSingleObserver.kt
@@ -1,11 +1,17 @@
 package pl.hypeapp.domain.usecase.base
 
 import io.reactivex.observers.DisposableSingleObserver
+import java.util.logging.Level
+import java.util.logging.Logger
 
 open class DefaultSingleObserver<T> : DisposableSingleObserver<T>() {
 
+    private val logger: Logger = Logger.getLogger("DefaultSingleObserver")
+
     override fun onSuccess(model: T) {}
 
-    override fun onError(error: Throwable) {}
+    override fun onError(error: Throwable) {
+        logger.log(Level.WARNING, error.message + " | \n" + error.stackTrace)
+    }
 
 }
